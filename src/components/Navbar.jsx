9899901bcd2a35b816795ec853a9909b3d23b84f
Junair <junairjunu869@gmail.com>
2023-04-28 16:57:16 +0530

    wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,8 @@
-import React, { useState } from 'react'
 import Reg from '../Img/regimg.png'
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
-  // let Links = [
-  //   {name: "Register", link:''},
-  //   {name: "Login", link:''}
-  // ]
   return (
     <div className='shadow-md w-full fixed top-0 left-0'>
       <div className='md:flex items-center justify-between bg-blue-100 py-4 md:px-10 px-7'>
@@ -17,13 +12,6 @@ const Navbar = () => {
           </span>
         </div>
         <ul className='md:flex md:items-center '>
-          {/* {
-            Links.map((link) => (
-              <li key={link.name} className='md:ml-8 text-3xl'>
-                  <a href={link.link} className='text-gray-400 hover:text-gray-900 duration-500'>{link.name}</a>
-              </li>
-            ))
-          } */}
           <Link to={'/register'}>
             <p onClick={() => {
               console.log('Register');
@@ -34,8 +22,6 @@ const Navbar = () => {
               console.log('Login');
             }} className='md:ml-8 text-3xl text-gray-400 hover:text-gray-900 duration-500'>Login</p>
           </Link>
-          {/* <li  className='md:ml-8 text-3xl text-gray-400 hover:text-gray-900 duration-500'>Register</li> */}
-          {/* <li className='md:ml-8 text-3xl text-gray-400 hover:text-gray-900 duration-500'>Login</li> */}
         </ul>
 
       </div>
--- src/data/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const data = axios.get(' https://panorbit.in/api/users.json').then(
-    (response) =>{
-        console.log(response);
-    }
-)
